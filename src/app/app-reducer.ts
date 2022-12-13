c7c2f190e609900cd89b46cbed0b7893c414a43e
grigorydobrenko <grigorydobrenko97@gmail.com>
2022-12-13 17:08:12 +0300

    Initial commit

---
+++ src/app/app-reducer.ts
@@ -0,0 +1,60 @@
+import {Dispatch} from 'redux'
+import {authAPI} from '../api/todolists-api'
+import {setIsLoggedInAC} from '../features/Login/auth-reducer'
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+const initialState = {
+    status: 'idle' as RequestStatusType,
+    error: null as null | string,
+    isInitialized: false
+}
+
+const slice = createSlice({
+    name: 'app',
+    initialState: initialState,
+    reducers: {
+        setAppStatusAC(state, action: PayloadAction<{ status: RequestStatusType }>) {
+            state.status = action.payload.status
+        },
+        setAppInitializedAC(state, action: PayloadAction<{ isInitialized: boolean }>) {
+            state.isInitialized = action.payload.isInitialized
+        },
+        setAppErrorAC(state, action: PayloadAction<{ error: string | null }>) {
+            state.error = action.payload.error
+        }
+    }
+})
+
+export const appReducer = slice.reducer
+console.dir(appReducer)
+
+export const {setAppInitializedAC, setAppErrorAC, setAppStatusAC} = slice.actions
+console.dir(slice.actions)
+
+
+
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+export type InitialStateType = {
+    // происходит ли сейчас взаимодействие с сервером
+    status: RequestStatusType
+    // если ошибка какая-то глобальная произойдёт - мы запишем текст ошибки сюда
+    error: string | null
+    // true когда приложение проинициализировалось (проверили юзера, настройки получили и т.д.)
+    isInitialized: boolean
+}
+
+
+export const initializeAppTC = () => (dispatch: Dispatch) => {
+    authAPI.me().then(res => {
+        if (res.data.resultCode === 0) {
+            dispatch(setIsLoggedInAC({value: true}));
+        } else {
+        }
+
+        dispatch(setAppInitializedAC({isInitialized: true}));
+    })
+}
+
+
+
+
